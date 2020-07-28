4b15c80f7d7589d061f0c6290c8b2d901472929e
Adamo Maisano <atwixtor@gmail.com>
2020-07-28 18:45:27 -0400

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -4,13 +4,16 @@ document.addEventListener("click", onClick, false)
 function onClick(ev) {
   let a = ev.target.closest('a');
 
+
   if (a) {
     const href = a.href;
 
     if (isJiraHref(href)) {
       ev.preventDefault();
+
+      // Choosy API at https://www.choosyosx.com/api
       const url = "x-choosy://open/" + href;
-      window.location.href = url;
+      window.open(url);
     }
   }
 }
@@ -19,5 +22,5 @@ function onClick(ev) {
  * Return `true` is the link is for Jira and should be opened in Choosy
  */
 function isJiraHref(href) {
-  return !/\.atlassian\.(net|com)/g.test(href) && !/^mailto:/.test(href);
+  return /\.atlassian\.(net|com)/g.test(href) && !/^mailto:/.test(href);
 }
